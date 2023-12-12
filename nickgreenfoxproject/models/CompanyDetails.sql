7c600731f88a6ae26e03fb3ed4213f94cbf019c0
Nick Taylor <nick.taylor.esl@gmail.com>
2023-12-12 13:56:05 +0000

    troubleshooting connection

--- nickgreenfoxproject/models/CompanyDetails.sql
+++
@@ -1,2 +0,0 @@
-SELECT Tags, L1_type, L2_type, L3_type, Organization, Repository_name, Repository_account, Open_source_available
-FROM nickgreenfoxproject.CompanyDetails.v3
---
+++ nickgreenfoxproject/models/staging/stg_CompanyDetails.sql
@@ -0,0 +1,2 @@
+SELECT Organization, Tags, L1_type, L2_type, L3_type, Repository_name, Repository_account, Open_source_available
+FROM nickgreenfoxproject.CompanyDetails.v3
